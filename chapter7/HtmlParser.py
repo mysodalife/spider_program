4a813ed4e9dd54b19c2c84fa707b54e8b4825b70
mysodalife <404997294@qq.com>
2018-10-14 20:50:21 +0800

    日常保存

--- chapter7/HtmlParser.py
+++ chapter7/HtmlParser.py
@@ -2,4 +2,17 @@
 # @Time         : 2018/10/14 16:31
 # @Author       : sodalife
 # @File         : HtmlParser.py
-# @Description  : 爬虫节点的html 解析器
+# @Description  : 爬虫节点的 html 解析器
+from bs4 import BeautifulSoup
+
+
+class HtmlParser(object):
+
+    def pareser(self, page_url, html_content):
+        soup = BeautifulSoup(html_content, 'html.parser')
+
+    def _get_new_urls(self, page_url: str, soup: BeautifulSoup):
+        pass
+
+    def _get_new_data(self, page_url: str, soup: BeautifulSoup):
+        pass
