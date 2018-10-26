f4ab6aa81f59050cbef79edc6a3139e1766b2210
mysodalife <404997294@qq.com>
2018-10-26 16:21:39 +0800

    "add requirements.txt"

---
+++ chapter10/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time         : 2018/10/24 19:23
+# @Author       : sodalife
+# @File         : __init__.py.py
+# @Description  : Web终端协议
---
+++ chapter10/baidu_login.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time         : 2018/10/26 10:12
+# @Author       : sodalife
+# @File         : baidu_login.py
+# @Description  : 模拟登陆百度云 这里涉及到加密数据的处理
+import base64
+import json
+import re
